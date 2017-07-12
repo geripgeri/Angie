cd063645db59a8ec9dd4d956ce4ec3c29d97dff8
Gergely Peidl <gergely@peidl.net>
2017-07-12 20:49:14 +0200

    Read BMP-180 Pressure in hPa instead of Pa #6

--- scripts/BMP180/pressure.py
+++ scripts/BMP180/pressure.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 import Adafruit_BMP.BMP085 as BMP085
 sensor = BMP085.BMP085()
-print('{0:0.2f}'.format(sensor.read_pressure()))
+print('{0:0.2f}'.format(sensor.read_pressure()/100))
